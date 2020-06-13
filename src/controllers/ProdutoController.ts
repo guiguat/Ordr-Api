367c722117c38da538aba9e9d1f5b380a8df03ec
guiguat <guaturadzn@gmail.com>
2020-06-13 00:53:12 -0300

    handle errors

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -1,64 +1,78 @@
 import knex from '../database/connection';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 class ProdutoController{
-   async create(req:Request,res:Response){
-      const { nome, preco, estoque, tipo } = req.body;
 
-      await knex('produto').insert({
-          nome, 
-          preco,
-          estoque,
-          tipo
-      })
-
-      return res.json({
-          message: "Cadastro concluido!"
-      });
-  }
-  async index(req:Request, res:Response){
-      if(req.query.id != undefined){
-          const [produto] = await knex('produto').select('*').where({
-              id: req.query.id
-          });
-          return res.json(produto);
-      }
-      else{
-          const produto = await knex('produto').select('*').orderBy('estoque');
-          return res.json(produto);
-      }
+   async create(req:Request,res:Response, next:NextFunction){
       
-  }
-  async edit(req:Request, res:Response){
-      let erro = null;
+      const { nome, preco, estoque, tipo } = req.body;
       try {
-          const { id, nome, preco, estoque, tipo } = req.body;
-          await knex('produto').where({id}).update({
-              nome,
-              preco,
-              estoque,
-              tipo
-          })
+         
+         await knex('produto').insert({
+            nome, 
+            preco,
+            estoque,
+            tipo
+         })
+
+         return res.json({
+               message: "Cadastro concluido!"
+         });
       } catch (error) {
-          erro = error;
-          console.log(error);
+         next(error);
       }
-      return erro? res.json({message:"Erro ao editar informações"}) : res.json({message:"Informacoes editadas com sucesso!"});
-  }
-  async delete(req:Request, res:Response){
-      const { id } = req.query;
-      await knex('produto').where({id}).delete();
-      return res.json({message:"Informacoes deletadas com sucesso!"});
-  }
-  async estoque(req:Request,res:Response){
-      let erro = null;
+
+   }
+
+   async index(req:Request, res:Response, next:NextFunction){
+  
       try {
-          const { id, estoque } = req.body;
-          await knex('produto').where({id}).update({ estoque })
+         const produto = await knex('produto').select('*').orderBy('estoque');
+         return res.json(produto);
       } catch (error) {
-          erro = error;
-          console.log(error);
+         next(error);
       }
-      return erro? res.json({message:"Erro ao editar informações"}) : res.json({message:"Informacoes editadas com sucesso!"});
-  }
+
+   }
+
+   async edit(req:Request, res:Response, next:NextFunction){
+
+      try {
+         const { id, nome, preco, estoque, tipo } = req.body;
+         await knex('produto').where({id}).update({
+            nome,
+            preco,
+            estoque,
+            tipo
+         })
+         return res.json({message:"Informacoes editadas com sucesso!"});
+      } catch (error) {
+         next(error);
+      }
+
+   }
+
+   async delete(req:Request, res:Response, next:NextFunction){
+
+      try {
+         const { id } = req.query;
+         await knex('produto').where({id}).delete();
+         return res.json({message:"Informacoes deletadas com sucesso!"});
+      } catch (error) {
+         next(error);
+      }
+
+   }
+
+   async estoque(req:Request,res:Response, next:NextFunction){
+
+      try {
+         const { id, estoque } = req.body;
+         await knex('produto').where({id}).update({ estoque });
+         res.json({message:"Informacoes editadas com sucesso!"});
+      } catch (error) {
+          next(error);
+      }
+
+   }
 }
 export default ProdutoController;
