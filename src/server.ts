ae7aac1460d1e73715725a22a61dfaf05240bfcb
guiguat <guaturadzn@gmail.com>
2020-06-13 00:42:26 -0300

    add error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import routes from './routes';
-import path from 'path';
 import cors from 'cors';
-
+interface ResponseError extends Error {
+    status?: number;
+  }
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);    
+
+app.use((error:ResponseError, req:Request, res:Response, next:NextFunction)=>{
+    res.status(error.status || 500);
+    res.json({
+        message: error.message
+    });
+})
 
 app.listen(3333);
