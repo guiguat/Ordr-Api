640209830bc44f0777a156bcbb2fd594201bd0a9
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-10 13:54:27 -0300

    Add sale controller

---
+++ src/controllers/SaleController.ts
@@ -0,0 +1,54 @@
+import knex from '../database/connection';
+import { Request, Response, NextFunction } from 'express';
+
+interface Product{
+    id:number,
+    name:string,
+    price:number,
+    stock:number,
+    type:string
+}
+
+class SaleController{
+
+    async index(req:Request, res:Response, next:NextFunction){
+
+        try {
+
+            const costumer_id = req.query.costumer_id;
+            
+            const orders = await knex('account').where({costumer_id}).select('*');
+            
+            res.json(orders);
+
+        } catch (error) {
+            next(error);
+        }
+
+    }
+
+    async create(req:Request, res:Response, next:NextFunction){
+
+        try {
+            const { products, costumer_id } = req.body;
+            products.map(async (product:Product) => {
+
+                await knex('account').insert({
+                    order:product.name,
+                    price:product.price,
+                    costumer_id
+                })
+
+            })
+            return res.json({
+                message_pt:"Pedido(s) enviado(s) com sucesso!",
+                message:"Order(s) successfully sent!"
+            });
+        } catch (error) {
+            next(error);
+        }
+
+    }
+   
+}
+export default SaleController;
