a0dba7df8a9fc1a68b5fd040bfa6791f8fdb5ffa
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-17 16:06:52 -0300

    create discount stock function

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -75,8 +75,8 @@ class ProductController{
 
       try {
          let { id, stock } = req.body;
-         const [previousStock] = await knex('product').where({id}).select("stock");
-         stock += previousStock.stock;
+         const [item] = await knex('product').where({id}).select("stock");
+         stock += item.stock;
          await knex('product').where({id}).update({ stock });
          res.json({
             message_pt:"Produto estocado com sucesso!",
@@ -86,6 +86,25 @@ class ProductController{
          next(error);
       }
 
+   }
+
+   async discountStock(req:Request, res:Response, next:NextFunction){
+      try {
+         let ids = req.query.ids;
+         ids = ids?.toString().split(",");
+         ids?.forEach(async (id)=>{
+            let stock = 0;
+            const [item] = await knex('product').where({id}).select("stock");
+            if(item.stock > 0) stock = item.stock - 1 ;
+            await knex('product').where({id}).update({ stock });
+         })
+         res.json({
+            message_pt:"Produto estocado com sucesso!",
+            message:"Product successfully stocked!"
+         });
+      } catch (error) {
+         next(error);
+      }
    }
    
 }
